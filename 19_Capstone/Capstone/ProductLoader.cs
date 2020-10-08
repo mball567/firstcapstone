666cb877a4728f84c747a0e9ba7901b476fbd67b
Michael Ball <mball567@gmail.com>
2020-10-08 13:32:30 -0400

    Finished Product classes

---
+++ 19_Capstone/Capstone/ProductLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public class ProductLoader
+    {
+        public static IEnumerable<Product> LoadProducts(string filePath)
+        {
+            using (StreamReader productList = new StreamReader(filePath))
+            {
+                while (!productList.EndOfStream)
+                {
+                    string input = productList.ReadLine();
+                    string[] fields = input.Split("|");
+                    string productName = fields[0];
+                    decimal price = decimal.Parse(fields[1]);
+                    string category = fields[2];
+                    Product prod = new Product(productName, price, category);
+                    products.Add(prod);
+                }
+            }
+        }
+    }
+}
